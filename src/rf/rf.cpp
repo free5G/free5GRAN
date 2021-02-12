bdbbd38edaf9bf3f315270637d9b7b3a269dd3bf
Aymeric de Javel <aymeric@dejavel.fr>
2021-02-12 11:00:05 +0100

    License update to 2020-2021

--- src/rf/rf.cpp
+++ src/rf/rf.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Telecom Paris
+ * Copyright 2020-2021 Telecom Paris
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
